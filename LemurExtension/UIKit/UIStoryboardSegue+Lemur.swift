b858f8e5e6e3f2f098f871f34274265dd32ecd98
callmewhy <whywanghai@gmail.com>
2015-10-28 16:57:23 +0800

    add UIStoryboardSegue

---
+++ LemurExtension/UIKit/UIStoryboardSegue+Lemur.swift
@@ -0,0 +1,28 @@
+//
+//  UIStoryboardSegue+Lemur.swift
+//  LemurExtension
+//
+//  Created by why on 10/28/15.
+//  Copyright © 2015 Lemur. All rights reserved.
+//
+
+import UIKit
+
+protocol SegueHandlerType {
+    typealias SegueIdentifier: RawRepresentable
+}
+
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+    func performSegueWithIdentifier(identifier: SegueIdentifier, sender: AnyObject?) {
+        performSegueWithIdentifier(identifier.rawValue, sender: sender)
+    }
+    
+    func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
+        guard
+            let identifier = segue.identifier,
+            let segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+                fatalError("INVALID SEGUE IDENTIFIER \(segue.identifier)")
+        }
+        return segueIdentifier
+    }
+}
